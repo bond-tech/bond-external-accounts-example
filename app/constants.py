5e8b9b18224183b2da084814abb3c9af28762b04
adityabond <59890741+adityabond@users.noreply.github.com>
2021-02-04 12:02:58 -0800

    plaid sdk

--- app/constants.py
+++ app/constants.py
@@ -14,8 +14,8 @@ bond_host = "https://sandbox.bond.tech"
 sdk_docs = "https://docs.bond.tech/docs/retrieve-card-details-set-pins-and-reset-pins"
 api_docs = "https://docs.bond.tech/reference"
 
-PLAID_CLIENT_ID="5f91a184df1def00129064d3"
-PLAID_SECRET="f82599de2db1661a229e24854dff14"
-PLAID_PRODUCTS="transactions"
-PLAID_COUNTRY_CODES="US"
-PLAID_ENV="sandbox"
+PLAID_CLIENT_ID=environ.get("PLAID_CLIENT_ID","5f91a184df1def00129064d3")
+PLAID_SECRET=environ.get("PLAID_SECRET","f82599de2db1661a229e24854dff14")
+PLAID_PRODUCTS=environ.get("PLAID_PRODUCTS","transactions")
+PLAID_COUNTRY_CODES=environ.get("PLAID_COUNTRY_CODES","US")
+PLAID_ENV=environ.get("PLAID_ENV", "sandbox")
